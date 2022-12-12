3cd6873533ae5e4db056cff73933eedf7fcf9d08
Keith Ma <keithchezzy@gmail.com>
2022-12-11 16:12:36 -0800

    commit tree 7fbad2725c7bade6ea74d14ca5ec64a8d7564a19

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
-# Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet name
+  puts 'Hello, ' + name + '!'
+end
+greet 'Naureen'
+
+def greet_with_default name='programmer'
+  puts 'Hello, ' + name + '!'
+end
+greet_with_default
+
+def add num1, num2
+  return num1 + num2
+end
+add 2, 5
+
+
+def halve(num)
+  return nil unless num.class == Integer
+  num/ 2
+end
